263c73623b5cf01f631607d39277649a862e874b
mhco0 <mhco@cin.ufpe.br>
2022-11-08 16:02:20 -0300

    Adding grid initialization

--- src/engine/grid/grid.cpp
+++ src/engine/grid/grid.cpp
@@ -1,5 +1,7 @@
-#include <assert.h>
 #include <engine/grid.h>
+#include <algorithm>
+#include <random>
+#include <assert.h> // or <cassert>
 
 namespace minesweeper{
 
@@ -11,14 +13,14 @@ grid::grid(const int& width, const int height){
     this->m_bombs = 0;
     this->m_started = false;
 
-    if(!this->m_width or !this->m_height) return;
+    if(not this->m_width or not this->m_height) return;
 
     this->m_cells = new cell[this->m_cells_sz]();
 }
 
 grid::~grid(){
     if(this->m_cells){
-        delete this->m_cells;
+        delete[] this->m_cells;
     }
 }
 
@@ -33,11 +35,92 @@ void grid::init(const grid_click_t& initial_click){
     if(this->m_started) return;
 
     // saves initial click as safe spot
+    size_t click_index = initial_click.x * this->m_width + initial_click.y;
+
     // randomize bombs into grid position
+    std::random_device dev;
+    std::seed_seq seed {dev(), dev(), dev(), dev(), dev(), dev(), dev(), dev()};
+    std::mt19937 eng(seed);
+
+    int* cells_index = new int[this->m_cells_sz - 1]();
+    
+    for(size_t i = 0, j = 0; i < this->m_cells_sz - 1; i++, j++){
+        if(j == click_index) j++;
+
+        cells_index[i] = j;
+    }
+
+    std::shuffle(cells_index, cells_index + this->m_cells_sz - 1, eng);
+
+    for(size_t i = 0, j = 0; i < this->m_bombs and j < this->m_cells_sz; i++, j++){
+        this->m_cells[cells_index[j]].set_type(cell_type::BOMB);
+    }
+
     // process each cell on grid based on bomb already defined positions
-    // if the click is a empty cell expand the grid view (mark cells as clicked)
+
+    /*
+        -------------
+        |B|2|1|1|B|1|
+        |2|B|1|1|1|1|
+        |1|1|1| | | |
+        | | | | | | |
+        | |1|2|3|2|1|
+        | |1|B|B|B|1|
+        -------------
+        
+        For each cell we need to check the bombs on each position on the cell next to it. 
+        We treat each cell on grid as a array, so to match the index of a cell we need to 
+        save the index of the array. Ex:
+
+        on position (2, 2) the index on the grid will be 2 * 6 + 2 = 14, so we need to convert
+        all the positions (1, 2), (3, 2), (2, 3), (2, 1), (1, 1), (3, 3), (1, 3) and (3, 1) in the 
+        array index positions. 
+    */
+
+    for(size_t i = 0; i < this->m_height; i++){
+        for(size_t j = 0; j < this->m_width; j++){
+            size_t cell_index = i * this->m_width + j;
+
+            if(this->m_cells[cell_index].type() == cell_type::BOMB) continue;
+            else{
+                // checks neightboards
+                int neighboards[6] = {
+                    (i - 1) * this->m_width + j,
+                    i * this->m_width + (j - 1),
+                    (i + 1) * this->m_width + j,
+                    i * this->m_width + (j + 1),
+                    (i - 1) * this->m_width + (j - 1),
+                    (i - 1) * this->m_width + (j + 1),
+                    (i + 1) * this->m_width + (j - 1),
+                    (i + 1) * this->m_width + (j + 1),
+                };
+
+                int bombs_next = 0;
+
+                for(short i = 0; i < 6; i++){
+                    if(neighboards[i] < 0 or neighboards[i] >= this->m_cells_sz) continue;
+                    
+                    if(this->m_cells[neighboards[i]].type() == cell_type::BOMB){
+                        bombs_next++;
+                    }
+                }
+
+                if(bombs_next){
+                    this->m_cells[cell_index].set_type(cell_type::NUMERED);
+                    this->m_cells[cell_index].set_bombs_next(bombs_next);
+                }
+            }
+        }
+    }
 
     this->m_started = true;
+
+    if(cells_index){
+        delete[] cells_index;
+    }
+
+    // process the initial click
+    this->process(initial_click);
 }
 
 void grid::process(const grid_click_t& click){
@@ -46,6 +129,7 @@ void grid::process(const grid_click_t& click){
 
     if(!this->m_started){
         this->init(click);
+        return;
     }
 
     size_t index = click.x * this->m_width + click.y;
@@ -55,8 +139,9 @@ void grid::process(const grid_click_t& click){
         if(this->m_cells[index].marked()) // add here the possibility of unmark the cell
         else this->m_cells[index].mark();
     }else{
-        this->m_cells[index].click();
+        if (not this->m_cells[index].marked()) this->m_cells[index].click();
         // expands grid here
+
     }
 
     return;
